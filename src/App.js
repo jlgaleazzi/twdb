d112708e285b11fa5a5c63b47f240b5cc3e0ccb5
Juan Galeazzi <jgaleazzi@gmail.com>
2020-07-30 12:46:25 -0500

    logic working

--- src/App.js
+++ src/App.js
@@ -19,7 +19,9 @@ function App() {
         let uqEmails = getUniqueEmails(mailArray);
         setUniqueEmails(uqEmails);
         return uqEmails;
-      });
+      })
+      .then((uniques => getCountPerDomain(uniques))
+      )
   };
   const csvToArray = (csv) => {
     let emails = csv.split("\n");
@@ -27,8 +29,8 @@ function App() {
   };
 
   const getUniqueEmails = (mails) => {
-    var dict = {};
-    var uniqueEmails = [];
+    let dict = {};
+    let uniqueEmails = [];
     for (let i = 0; i < mails.length; i++) {
       if (dict[mails[i]] !== mails[i]) {
         uniqueEmails.push(mails[i]);
@@ -38,10 +40,26 @@ function App() {
   };
 
   const getCountPerDomain = (uniqueEmails) => {
-    // parse each mail
-    // split domain at @
-    // return user count
+
+    let dict = {}
+    for (let i = 0; i < uniqueEmails.length; i++) {
+      let email = uniqueEmails[i].split("@");
+      if (email != "") {
+        let domain = email[1].split('.')[0];
+        if (dict[domain] === undefined) {
+
+          dict[domain] = 1;
+        } else {
+
+          dict[domain] += 1;
+        }
+      }
+    }
+    setAddressPerDomain(dict);
+    return dict;
   };
+
+
 
   return (
     <div className="App">
